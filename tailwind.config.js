f6715b1b1cc9ed17ffcb68e15762a583c8a225c7
jSagg <jaiaggarwal018@gmail.com>
2025-08-10 20:07:39 +0530

    Fix: Downgrade to stable package versions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // This line is crucial
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
